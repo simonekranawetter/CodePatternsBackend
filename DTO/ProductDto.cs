b167796df8316f992b8967b46b98949799c530c7
Simone Kranawetter <simonekranawetter@gmail.com>
2022-12-06 21:09:10 +0100

    :pencil2: add comments for upcoming changes

--- DTO/ProductDto.cs
+++ DTO/ProductDto.cs
@@ -1,9 +1,10 @@
 ﻿namespace CodePatternsBackend.DTO
 {
     public class ProductDto
-    {
+    {// TODO, change rating to number and add string picture
         public int Id { get; set; }
         public string? Rating { get; set; }
+        //TODO, rename to ProductName 
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public decimal Price { get; set; }
